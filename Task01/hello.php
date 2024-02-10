b2fde5e1c4e28a122e61cf864425516bcc5a24de
SAMURAI <vip.13rus@bk.ru>
2024-02-10 14:09:20 +0300

    Task

---
+++ Task01/hello.php
@@ -0,0 +1,7 @@
+<?php
+	date_default_timezone_set("Europe/Moscow");
+	 $a = date('d.m.Y H:i:s');
+   echo $a;
+   echo " Tazin Alexandr";
+
+?>
---
+++ Task01/start.bat
@@ -0,0 +1,3 @@
+#!/bin/bash
+C:\Users\SAMURAI\Desktop\PHP\php "C:\Users\SAMURAI\Git\302_OOP_Tazin_AN\Task01\hello.php"
+pause
